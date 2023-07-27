8d2244ac264643f0516631cf9cd0d64c579340fb
Dmitriy151 <nova151kra@gmail.com>
2023-07-27 20:40:00 +0600

    27.07

---
+++ script.js
@@ -0,0 +1,10 @@
+const names= ['Денис','Егор','Пётер','Иван','Олег'];
+undefined
+let searchName='Егор';
+undefined
+for (let name of names) {
+    if (name===searchName) {
+        console.log(name);
+        break;
+    }
+}
