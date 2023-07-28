cabdeebdaea3a42605ca1d570e1b3273f8ed1eed
Dmitriy151 <nova151kra@gmail.com>
2023-07-28 12:49:30 +0600

    28,07

--- script.js
+++ script.js
@@ -11,3 +11,13 @@ for (let name of names) {
 
 names.indexOf(searchName);
 names.indexOf('');
+console.log(names.indexOf(''));
+console.log(names.indexOf(searchName));
+
+if (names.indexOf('Денис')==!1) {
+  console.log('нашли элемент');
+}
+
+if (names.includes('Денис')) {
+  console.log('нашли Дениса');
+}
