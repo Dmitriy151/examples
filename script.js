b38e3138eaa300d2699a10d39c576a644754e4aa
Dmitriy151 <nova151kra@gmail.com>
2023-08-23 22:54:53 +0600

    23.08. 'evning'

--- script.js
+++ script.js
@@ -128,3 +128,15 @@ olga.addScore(9);
 olga.addScore(10);
 olga.addScore(10);
 console.log(olga.calculateAverage());
+
+
+
+function init() {
+  let userName='Олег';
+function showGreeting(name) {
+  console.log(`Привет ${name}`);
+}
+showGreeting(userName);
+}
+init();
+
